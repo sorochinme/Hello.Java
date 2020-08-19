ac210cf6c3a01f3538548ec7428c199c881eba6c
sorochinme <69909294+sorochinme@users.noreply.github.com>
2020-08-19 10:20:17 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/***********************************************************************
+ * Lab 01
+ ***********************************************************************
+ * Megan Sorochin
+ * 8/18/2020
+ * CMSC255 Section 003
+ **********************************************************************/
+
+public class Hello {
+
+	 public static void main(String[] args) {
+
+	 	System.out.println("Megan Sorochin");
+	 	System.out.println("Lab 01");
+	 	System.out.println("Computer Science Lab 01");
+	 	System.out.println("08/19/2020");
+	 	System.out.println("CMSC255 Section 003");
+	 	System.out.println();
+	 }
+}
